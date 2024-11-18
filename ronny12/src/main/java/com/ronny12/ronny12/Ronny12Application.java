5df33f2a4f759c5e5d33a37feba4fd56a31782af
Ronny Panduro <100005222+ronnypanduro@users.noreply.github.com>
2024-11-18 12:17:17 -0500

    agregando las dependencias

---
+++ ronny12/src/main/java/com/ronny12/ronny12/Ronny12Application.java
@@ -0,0 +1,13 @@
+package com.ronny12.ronny12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ronny12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ronny12Application.class, args);
+	}
+
+}
---
+++ ronny12/src/test/java/com/ronny12/ronny12/Ronny12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronny12.ronny12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ronny12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
